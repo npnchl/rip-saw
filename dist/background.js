e55e90304f5d8e47d3d92ea3787a0fb3a197b84e
npnchl <np.40027072@gmail.com>
2025-08-01 15:32:07 +0100

    feat(blocking): add local storage
    Lay the foundation for user input on sites to block:
    - Use local storage to store user-defined rules.
    - Implement logic to retrieve user-defined rules from local storage and
    load them into the extension's dynamic ruleset.

--- dist/background.js
+++ dist/background.js
@@ -998,22 +998,42 @@
   var require_background = __commonJS({
     "src/background.js"() {
       var browser = require_browser_polyfill();
-      var newRule = {
-        id: 1,
-        priority: 1,
-        condition: {
-          urlFilter: "youtube.com",
-          resourceTypes: ["main_frame"]
-        },
-        action: {
-          type: "block"
-        }
-      };
+      browser.storage.local.clear();
+      browser.storage.local.set({
+        denyByDefault: [
+          {
+            id: 1,
+            priority: 1,
+            action: {
+              type: "block"
+            },
+            condition: {
+              urlFilter: "reddit.com",
+              resourceTypes: ["main_frame"]
+            }
+          },
+          {
+            id: 2,
+            priority: 1,
+            action: {
+              type: "block"
+            },
+            condition: {
+              urlFilter: "youtube.com",
+              resourceTypes: ["main_frame"]
+            }
+          }
+        ]
+      });
       browser.declarativeNetRequest.getDynamicRules().then((rules) => {
         let ruleIds = rules.map((rule) => rule.id);
         browser.declarativeNetRequest.updateDynamicRules({
-          removeRuleIds: ruleIds,
-          addRules: [newRule]
+          removeRuleIds: ruleIds
+        });
+      });
+      browser.storage.local.get("denyByDefault").then((dataObj) => {
+        browser.declarativeNetRequest.updateDynamicRules({
+          addRules: dataObj.denyByDefault
         });
       });
       browser.declarativeNetRequest.getDynamicRules().then((rules) => {
